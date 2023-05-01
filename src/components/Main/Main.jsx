8226611bb67238c792b13e2e19b358c4044d11a9
mhemon <mhemon02@gmail.com>
2023-05-02 02:04:54 +0600

    footer added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../Shared/NavBar/NavBar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <NavBar/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 };
---
+++ src/components/Shared/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-light">
+            <Container>
+                <Row className='py-3 mb-2'>
+                    <Col md={6}>
+                        <img className='img-fluid footer-img-size' src="logo_white.png" alt="" />
+                        <h4>Chef's Recipe Hub</h4>
+                        <p>Cook up something new with our expert recipes.</p>
+                    </Col>
+                    <Col md={6}>
+                        <h4>Social Links</h4>
+                        <ul className="list-unstyled">
+                            <li><Link className='text-white'>Facebook</Link></li>
+                            <li><Link className='text-white'>Instagram</Link></li>
+                            <li><Link className='text-white'>Twitter</Link></li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <hr />
+                        <p className="text-center">&copy; {new Date().getFullYear()} Chef's Recipe Hub</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
