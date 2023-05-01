4d2b505342a9bec3dc7e6192e03a51de0dd3cc66
mhemon <mhemon02@gmail.com>
2023-05-02 03:23:32 +0600

    banner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import banner1 from '../../assets/banner01.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h3>This is home page</h3>
+        <div className='container'>
+            {/* banner section */}
+            <section className='mt-2 my-md-4'>
+                <Row>
+                    <Col md={6}>
+                        <img className='img-fluid' src={banner1} alt="" />
+                    </Col>
+                    <Col md={6}>
+                        <div className='p-2 px-md-4 py-md-4'>
+                            <h3 className='fst-italic'>Biryani specialist</h3>
+                            <h1 className='chef-name text-warning'>David <br /> Parker</h1>
+                            <p className='text-muted justify-text'>David Parker is a Biryani specialist with a passion for creating rich and flavorful dishes. With years of experience in the culinary arts, David has honed his skills to create the perfect balance of spices and textures in his Biryani recipes. His signature dishes are known for their aromatic flavors and mouth-watering appeal.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </section>
+            {/* Chef Section */}
+            <section>
+                <h2 className='text-center'>Chef's Section</h2>
+                <hr />
+                
+            </section>
         </div>
     );
 };
