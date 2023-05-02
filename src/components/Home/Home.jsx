82820c8a0d482fd9de7d59d22d6ece157102704f
mhemon <mhemon02@gmail.com>
2023-05-02 11:01:18 +0600

    Signature items added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -26,10 +26,57 @@ const Home = () => {
             </section>
             {/* Chef Section */}
             <section>
-                <h2 className='text-center'>Chef's Section</h2>
+                <h2 className='text-center fw-bold'>Chef's <span className='text-warning'>Section</span></h2>
                 <hr />
                 <Row xs={1} md={2} lg={3} className="g-4 py-2 mb-2">
                     {chefData.map(chef => <SingleChef key={chef.id} chef={chef}></SingleChef>)}
+                </Row>
+            </section>
+            {/* signature item section */}
+            <section className='mt-2 mt-lg-4'>
+                <h2 className='text-center fw-bold'>Signature <span className='text-warning'>items</span></h2>
+                <hr />
+                {/*signature items container */}
+                <Row xs={1} md={2} lg={3} className="g-4 py-2 mb-2">
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <div className='card-img'>
+                                <Card.Img className='img-fluid' variant="top" src="https://images.unsplash.com/photo-1515579171902-e0c5f918b32b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+                            </div>
+                            <Card.Body>
+                                <Card.Title>Dark Chocolate Truffles</Card.Title>
+                                <Card.Text className='text-justify'>
+                                Indulge in the rich and smooth flavor of our handcrafted dark chocolate truffles, made with the finest ingredients and expertly crafted for perfection.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <div className='card-img'>
+                                <Card.Img className='img-fluid' variant="top" src="https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" />
+                            </div>
+                            <Card.Body>
+                                <Card.Title>Margherita Pizza</Card.Title>
+                                <Card.Text className='text-justify'>
+                                Margherita Pizza is a classic Italian pizza made with a tomato sauce base, mozzarella cheese, and fresh basil. Simple yet delicious, it's a timeless favorite.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <div className='card-img'>
+                                <Card.Img className='img-fluid' variant="top" src="https://images.unsplash.com/photo-1619019187211-adf2f6119afd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+                            </div>
+                            <Card.Body>
+                                <Card.Title>Chicken Tenders</Card.Title>
+                                <Card.Text className='text-justify'>
+                                Chicken tenders are a popular dish made by breaded and deep-fried strips of chicken. They are crispy on the outside and juicy on the inside, perfect for dipping.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
             </section>
         </div>
